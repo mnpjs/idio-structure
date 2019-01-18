9613e6e168829b2794de52a6043240488becf04b
Anton <anton@adc.sh>
2019-01-18 18:22:57 +0300

    middleware

--- structure/src/server.js
+++ structure/src/server.js
@@ -2,11 +2,21 @@ import idio from '@idio/core'
 import initRoutes, { watchRoutes } from '@idio/router'
 
 const PROD = process.env.NODE_ENV == 'production'
+const FRONT_END = process.env.FRONT_END || 'https://website.co'
 
 export default async ({
   port, watch = !PROD,
 }) => {
   const { router, middleware, app, url } = await idio({
+    cors: { use: true,
+      origin: PROD && [FRONT_END],
+      config: { credentials: true } },
+    compress: { use: true },
+    static: { use: true, root: 'static', config: {
+      maxage: PROD ? 1000 * 60 * 60 * 60 * 24 : 0,
+    } },
+    session: { keys: [process.env.SESSION_KEY] },
+    bodyparser: {},
   }, { port })
   const w = await initRoutes(router, 'routes', {
     middleware,
